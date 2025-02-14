8e8bf863f286faa4c57bf471c667a0421c73a51c
MaskedByte660 <codingtechinfo01@gmail.com>
2025-02-14 11:54:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ function insert_Row() {
     //first create new row using insertRow
 	let newRow = table.insertRow(0);
 	//create new cell using insertCell
-	let cell1 = newRow.insertCell(0);
-	let cell2 = newRow.insertCell(1);
+	// let cell1 = newRow.insertCell(0);
+	// let cell2 = newRow.insertCell(1);
+
+	let cell1 = document.createElement("td");
+	let cell2 = document.createElement("td");
 
 	cell1.textContent = "New Cell1"
 	cell2.textContent = "New Cell2"
+
+	newRow.append(cell1, cell2);
 	//give innerText New Cell1
 
 }
