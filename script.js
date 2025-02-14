144e72fcbe667c6ef7d319e1a2f91f9f063d512c
MaskedByte660 <codingtechinfo01@gmail.com>
2025-02-14 11:53:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	 //access table first
+	let table = document.getElementById("sampleTable");
+    //first create new row using insertRow
+	let newRow = table.insertRow(0);
+	//create new cell using insertCell
+	let cell1 = newRow.insertCell(0);
+	let cell2 = newRow.insertCell(1);
+
+	cell1.textContent = "New Cell1"
+	cell2.textContent = "New Cell2"
+	//give innerText New Cell1
+
 }
